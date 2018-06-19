64b9d80ae15140906addd4b527478205b2c0be36
Greg Lim <limjunqu@gmail.com>
2018-06-19 16:01:43 +0800

    Update auth.guard.ts

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -2,8 +2,9 @@ import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot,RouterStateSnapshot, Router} from '@angular/router';
 import { LoginService } from './login.service';
 import { Observable } from 'rxjs';
-import 'rxjs/add/operator/take';
-import 'rxjs/add/operator/map';
+//import 'rxjs/add/operator/take';
+//import 'rxjs/add/operator/map';
+import { take,map } from 'rxjs/operators';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
@@ -11,6 +12,7 @@ export class AuthGuard implements CanActivate {
 
     }
 
+    /*
     canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot):Observable<boolean>{
         return this.loginService.isLoggedIn
             .take(1)
@@ -21,6 +23,19 @@ export class AuthGuard implements CanActivate {
                 }
                 return true;                
             })
+    }*/
+    // updated for Angular 6
+    canActivate(next:ActivatedRouteSnapshot,state: RouterStateSnapshot):Observable<boolean>{
+        return this.loginService.isLoggedIn
+		.pipe(take(1),
+                map((isLoggedIn:boolean) => {
+                  if(!isLoggedIn){
+                    this.router.navigate(['/login']);
+                    return false;
+                  }
+                  return true;                
+                })
     }
+    
     
 }
