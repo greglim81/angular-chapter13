765ed8ad503e7bed4cb9e1b4e319df0ed4355ae4
Greg Lim <limjunqu@gmail.com>
2018-06-19 15:55:23 +0800

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { LoginService } from './login.service';
-import { Observable } from 'rxjs/Observable'; 
+import { Observable } from 'rxjs'; 
 
 @Component({
   selector: 'app-root',
