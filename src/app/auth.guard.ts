57e68f1eedf06d869b9da8fab654f98134d31870
Greg Lim <limjunqu@gmail.com>
2018-06-19 15:55:05 +0800

    Update auth.guard.ts

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot,RouterStateSnapshot, Router} from '@angular/router';
 import { LoginService } from './login.service';
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs';
 import 'rxjs/add/operator/take';
 import 'rxjs/add/operator/map';
 
