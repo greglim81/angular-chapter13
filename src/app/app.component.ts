0bc3bf916a2ea223c0ad6a2e95b40c85dc8f71e7
Greg Lim <limjunqu@gmail.com>
2020-04-06 10:53:37 +0800

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,33 +5,34 @@ import { Observable } from 'rxjs';
 @Component({
   selector: 'app-root',
   template: `
-    <nav class="navbar navbar-default">
-    <div class="container-fluid">   
-      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-        <ul class="nav navbar-nav">                                    
-            <li><a (click)="onLogout()" *ngIf="isLoggedIn | async">Logout</a></li>
-            <li><a routerLink="login" *ngIf="!(isLoggedIn | async)">Login</a></li>            
-            <li><a routerLink="signup" *ngIf="!(isLoggedIn | async)">Sign Up</a></li>            
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">        
+            <a class="nav-link" (click)="onLogout()" *ngIf="isLoggedIn | async">Logout<span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">        
+            <a class="nav-link" routerLink="login" *ngIf="!(isLoggedIn | async)">Login</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" routerLink="signup" *ngIf="!(isLoggedIn | async)">Sign Up</a>
+          </li>
         </ul>
-      </div><!-- /.navbar-collapse -->
-    </div><!-- /.container-fluid -->
-    </nav> 
-    <router-outlet></router-outlet>        
-  `
+      </div>
+    </nav>
+    <router-outlet></router-outlet>`
 })
 export class AppComponent {
   isLoggedIn: Observable<boolean>;
-
   constructor(private loginService: LoginService){    
   }
 
   ngOnInit(){
     this.loginService.getCurrentUser();
-    this.isLoggedIn = this.loginService.isLoggedIn;        
+    this.isLoggedIn = this.loginService.isLoggedIn;            
   }
 
   onLogout(){
     this.loginService.logout();
-  }
-  
+  } 
 }
