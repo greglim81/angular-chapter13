e1b9e8294b389a35d269eb9ae4a5a7b03e662aa0
Greg Lim <limjunqu@gmail.com>
2020-04-06 10:57:43 +0800

    Update app.routing.ts

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,16 +1,17 @@
 import { RouterModule } from '@angular/router';
-import { UsersComponent } from './users.component';
+import { UserComponent } from './user.component';
 import { UserFormComponent } from './user-form.component';
 import { LoginComponent } from './login.component';
 import { SignupComponent } from './signup.component';
 import { AuthGuard } from './auth.guard';
 
 export const routing = RouterModule.forRoot([
-    { path:'', component:UsersComponent, canActivate:[AuthGuard] },
+    { path:'', component:UserComponent, canActivate:[AuthGuard] },
     { path:'add',component:UserFormComponent, canActivate:[AuthGuard]}, 
     { path:'add/:id', component: UserFormComponent,canActivate:[AuthGuard] },
-    { path:'login',component:LoginComponent },
-    { path:'login/:invalidLoginMessage',component:LoginComponent },
+
+    { path:'login', component:LoginComponent },
+    { path:'login/:invalidLoginMessage', component:LoginComponent },
     { path:'signup',component:SignupComponent },
-    { path:'signup/:invalidLoginMessage',component:SignupComponent }
+    { path:'signup/:invalidLoginMessage',component:SignupComponent }    
 ]);
